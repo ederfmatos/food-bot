1dba5cb7129e38b349623cb3d411d40de9c849a9
ederfmatos <ederfmatos@gmail.com>
2020-08-16 14:00:15 -0300

    FEAT: Initial structure for server

---
+++ server/src/welcome.js
@@ -0,0 +1,25 @@
+import { version } from '../package.json';
+const logo = `
+                                                                                                                                                           
+                                                                       dddddddd                                                                                  
+FFFFFFFFFFFFFFFFFFFFFF                                                 d::::::d      BBBBBBBBBBBBBBBBB                                                    tttt          
+F::::::::::::::::::::F                                                 d::::::d      B::::::::::::::::B                                                ttt:::t          
+F::::::::::::::::::::F                                                 d::::::d      B::::::BBBBBB:::::B                                               t:::::t          
+FF::::::FFFFFFFFF::::F                                                 d:::::d       BB:::::B     B:::::B                                              t:::::t          
+  F:::::F       FFFFF   ooooooooooo        ooooooooooo         ddddddddd:::::d         B::::B     B:::::B     ooooooooooo        ooooooooooo     ttttttt:::::ttttttt    
+  F:::::F             oo:::::::::::oo    oo:::::::::::oo     dd::::::::::::::d         B::::B     B:::::B   oo:::::::::::oo    oo:::::::::::oo   t:::::::::::::::::t    
+  F::::::FFFFFFFFFF  o:::::::::::::::o  o:::::::::::::::o   d::::::::::::::::d         B::::BBBBBB:::::B   o:::::::::::::::o  o:::::::::::::::o  t:::::::::::::::::t    
+  F:::::::::::::::F  o:::::ooooo:::::o  o:::::ooooo:::::o  d:::::::ddddd:::::d         B:::::::::::::BB    o:::::ooooo:::::o  o:::::ooooo:::::o  tttttt:::::::tttttt    
+  F:::::::::::::::F  o::::o     o::::o  o::::o     o::::o  d::::::d    d:::::d         B::::BBBBBB:::::B   o::::o     o::::o  o::::o     o::::o        t:::::t          
+  F::::::FFFFFFFFFF  o::::o     o::::o  o::::o     o::::o  d:::::d     d:::::d         B::::B     B:::::B  o::::o     o::::o  o::::o     o::::o        t:::::t          
+  F:::::F            o::::o     o::::o  o::::o     o::::o  d:::::d     d:::::d         B::::B     B:::::B  o::::o     o::::o  o::::o     o::::o        t:::::t          
+  F:::::F            o::::o     o::::o  o::::o     o::::o  d:::::d     d:::::d         B::::B     B:::::B  o::::o     o::::o  o::::o     o::::o        t:::::t    tttttt
+FF:::::::FF          o:::::ooooo:::::o  o:::::ooooo:::::o  d::::::ddddd::::::dd      BB:::::BBBBBB::::::B  o:::::ooooo:::::o  o:::::ooooo:::::o        t::::::tttt:::::t
+F::::::::FF          o:::::::::::::::o  o:::::::::::::::o   d:::::::::::::::::d      B:::::::::::::::::B   o:::::::::::::::o  o:::::::::::::::o        tt::::::::::::::t
+F::::::::FF           oo:::::::::::oo    oo:::::::::::oo     d:::::::::ddd::::d      B::::::::::::::::B     oo:::::::::::oo    oo:::::::::::oo           tt:::::::::::tt
+FFFFFFFFFFF             ooooooooooo        ooooooooooo        ddddddddd   ddddd      BBBBBBBBBBBBBBBBB        ooooooooooo        ooooooooooo               ttttttttttt  
+                                    
+`;
+
+console.log(logo);
+console.log(`v${version}`);
